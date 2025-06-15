f3a2c8d5b908ac52f4260209babc49acaf314d49
Konrad Sypek <konrad.sypek@o2.pl>
2025-06-15 23:17:49 +0200

    added NIP validator

--- src/utils/Validators.java
+++ src/utils/Validators.java
@@ -10,6 +10,10 @@ public class Validators {
         return pesel != null && pesel.matches("\\d{11}");
     }
 
+    public static boolean isValidNIP(String NIP) {
+        return NIP != null && NIP.matches("\\d{10}");
+    }
+
     public static boolean isValidEmail(String email) {
         return email != null && email.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$");
     }
